ef3bc782aae5bb3a7f5430a125873a62dcd2cdf0
Maria Szawerna <98593377+Turtilla@users.noreply.github.com>
2022-04-08 10:41:23 +0200

    Update tree-explorer.py
    Adds an option to have the top X count be optional, and to retrieve features grouped up for every entry or counted individually.

--- tree-explorer.py
+++ tree-explorer.py
@@ -3,20 +3,28 @@ import argparse
 
 parser = argparse.ArgumentParser(description='Returns the counts of upos or xpos tags in a conllu file.')
 parser.add_argument('filename', help='The name or the path of the file you want to get the data from.')
-parser.add_argument('count', type=int, help='The top X tags to be displayed.')
+parser.add_argument('--count', type=int, help='The top X tags to be displayed.')
 parser.add_argument('--upos', action='store_true', help='Retrieve upos tag counts.')
 parser.add_argument('--xpos', action='store_true', help='Retrieve xpos tag counts.')
 parser.add_argument('--deprel', action='store_true', help='Retrieve deprel tag counts.')
+parser.add_argument('--feats', action='store_true', help='Retrieve features tag counts.')
+parser.add_argument('--indi_feats', action='store_true', help='Retrieve individual features tag counts.')
 args = parser.parse_args()
 
 def read_file(filename):
-    with open(filename,'r',encoding = 'utf-8') as f:
-        data = f.read()
-    sentences = parse(data)
+    '''Reads the data from the .conllu file and parses it using the conllu module. Returns a list of conllu objects.'''
+    if filename.endswith(".conllu"):
+        with open(filename,'r',encoding = 'utf-8') as f:
+            data = f.read()
+        sentences = parse(data)
+    else:
+        print("This type of file is not allowed!")
+        quit()
 
     return sentences
 
 def count_upos(sentences):
+    '''Counts the upos tags in the conllu sentences. Returns a list sorted from the largest item.'''
     count_dictionary = {}
     for sentence in sentences:
         for token in sentence:
@@ -37,6 +45,7 @@ def count_upos(sentences):
     return sorted_list
 
 def count_xpos(sentences):
+    '''Counts the xpos tags in the conllu sentences. Returns a list sorted from the largest item.'''
     count_dictionary = {}
     for sentence in sentences:
         for token in sentence:
@@ -57,6 +66,7 @@ def count_xpos(sentences):
     return sorted_list
 
 def count_deprel(sentences):
+    '''Counts the deprel tags in the conllu sentences. Returns a list sorted from the largest item.'''
     count_dictionary = {}
     for sentence in sentences:
         for token in sentence:
@@ -76,7 +86,61 @@ def count_deprel(sentences):
     
     return sorted_list
 
+def count_feats(sentences):
+    '''Counts the feats tags in the conllu sentences. Returns a list sorted from the largest item.'''
+    count_dictionary = {}
+    for sentence in sentences:
+        for token in sentence:
+            pos_tag = token['feats']
+            full_tag = []
+            if hasattr(pos_tag, '__len__'):
+                for k,v in pos_tag.items():
+                    pair = f'{k}={v}'
+                    full_tag.append(pair)
+                full_tag = tuple(full_tag)
+                if full_tag not in count_dictionary:
+                    count_dictionary[full_tag] = 1
+                else:
+                    count_dictionary[full_tag] += 1
+            else:
+                continue
+
+    count_list = []
+    for k,v in count_dictionary.items():
+        pair = (k,v)
+        count_list.append(pair)
+
+    sorted_list = sorted(count_list, reverse=True, key=lambda x: x[1])
+    
+    return sorted_list
+
+def count_individual_feats(sentences):
+    '''Counts the feats tags in the conllu sentences. Returns a list sorted from the largest item.'''
+    count_dictionary = {}
+    for sentence in sentences:
+        for token in sentence:
+            pos_tag = token['feats']
+            if hasattr(pos_tag, '__len__'):
+                for k,v in pos_tag.items():
+                    pair = f'{k}={v}'
+                if pair not in count_dictionary:
+                    count_dictionary[pair] = 1
+                else:
+                    count_dictionary[pair] += 1
+            else:
+                continue
+
+    count_list = []
+    for k,v in count_dictionary.items():
+        pair = (k,v)
+        count_list.append(pair)
+
+    sorted_list = sorted(count_list, reverse=True, key=lambda x: x[1])
+    
+    return sorted_list
+
 def top_x(sorted_list, count):
+    '''Prints the top X elements from a list of tuples.'''
     if len(sorted_list) < count:
         print(f'This list has less than {count} entries!')
         for i in range(0,len(sorted_list)):
@@ -90,23 +154,72 @@ def top_x(sorted_list, count):
             print(f'{ranking}. {k}: {v}')
     print('')
 
+def print_all(sorted_list):
+    '''Prints all the elements from a list of tuples.'''
+    for i in range(0,len(sorted_list)):
+        k, v = sorted_list[i]
+        ranking = str(i+1)
+        print(f'{ranking}. {k}: {v}')
+
+    print('')
+
 
 if __name__ == "__main__":
     filename = args.filename
-    count = args.count
     sentences = read_file(filename)
 
     if args.xpos:
-        print(f'Retrieving top {count} xpos counts from {filename}:')
-        sorted_list = count_xpos(sentences)
-        top_x(sorted_list, count)
+        if args.count:
+            count = args.count
+            print(f'Retrieving top {count} xpos counts from {filename}:')
+            sorted_list = count_xpos(sentences)
+            top_x(sorted_list, count)
+        else:
+            print(f'Retrieving all xpos counts from {filename}:')
+            sorted_list = count_xpos(sentences)
+            print_all(sorted_list)
+            
 
     if args.upos:
-        print(f'Retrieving top {count} upos counts from {filename}:')
-        sorted_list = count_upos(sentences)
-        top_x(sorted_list, count)    
+        if args.count:
+            count = args.count
+            print(f'Retrieving top {count} upos counts from {filename}:')
+            sorted_list = count_upos(sentences)
+            top_x(sorted_list, count)
+        else:
+            print(f'Retrieving all upos counts from {filename}:')
+            sorted_list = count_upos(sentences)
+            print_all(sorted_list) 
         
     if args.deprel:
-        print(f'Retrieving top {count} deprel counts from {filename}:')
-        sorted_list = count_deprel(sentences)
-        top_x(sorted_list, count)
+        if args.count:
+            count = args.count
+            print(f'Retrieving top {count} deprel counts from {filename}:')
+            sorted_list = count_deprel(sentences)
+            top_x(sorted_list, count)
+        else:
+            print(f'Retrieving all deprel counts from {filename}:')
+            sorted_list = count_deprel(sentences)
+            print_all(sorted_list)
+
+    if args.feats:
+        if args.count:
+            count = args.count
+            print(f'Retrieving top {count} feats counts from {filename}:')
+            sorted_list = count_feats(sentences)
+            top_x(sorted_list, count)
+        else:
+            print(f'Retrieving all feats counts from {filename}:')
+            sorted_list = count_feats(sentences)
+            print_all(sorted_list)
+
+    if args.indi_feats:
+        if args.count:
+            count = args.count
+            print(f'Retrieving top {count} individual feats counts from {filename}:')
+            sorted_list = count_individual_feats(sentences)
+            top_x(sorted_list, count)
+        else:
+            print(f'Retrieving all individual feats counts from {filename}:')
+            sorted_list = count_individual_feats(sentences)
+            print_all(sorted_list)
